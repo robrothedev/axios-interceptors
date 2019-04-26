91a27e5d5f67c7ecf72af5d5c1bf6502540837a1
Rob Rothe <rob@macpractice.com>
2019-04-26 14:08:05 -0500

    new http file

---
+++ src/Http.js
@@ -0,0 +1,42 @@
+/**
+ * Http.js
+ *
+ * Axios configuration for headers and request/response interceptors
+ */
+import axios from "axios";
+import PubSub from "pubsub-js";
+
+// base url for api requests
+const http = axios.create({
+  baseURL: "https://5c93afb84dca5d0014ad82b1.mockapi.io/api/"
+});
+
+// capture the last url in the event of a response error
+let lastUrl;
+
+http.interceptors.request.use(
+  // request successful
+  function(config) {
+    // set the last url in the event of a response error
+    lastUrl = config.url;
+    return config;
+  },
+  function(error) {
+    return Promise.reject(error);
+  }
+);
+
+http.interceptors.response.use(
+  // response successful
+  function(response) {
+    return response;
+  },
+
+  // response error so handle it
+  function(error) {
+    PubSub.publish("API_ERROR", { error: error.response, lastUrl: lastUrl });
+    return Promise.reject(error);
+  }
+);
+
+export default http;
